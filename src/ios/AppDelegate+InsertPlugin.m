aeb1d9a1ca5f0189597fabd5369f9448071d4093
Hieu Mai <znatuekid@gmail.com>
2017-12-05 14:34:31 +0700

    exchange function

--- src/ios/AppDelegate+InsertPlugin.m
+++ src/ios/AppDelegate+InsertPlugin.m
@@ -9,7 +9,10 @@
 {
     Method original =  class_getInstanceMethod(self, @selector(application:didFinishLaunchingWithOptions:));
     Method custom =    class_getInstanceMethod(self, @selector(application:insertDidFinishLaunchingWithOptions:));
+    Method original_open =  class_getInstanceMethod(self, @selector(application:openURL:sourceApplication:annotation:));
+    Method custom_open =    class_getInstanceMethod(self, @selector(application:insertOpenURL:sourceApplication:annotation:));
     method_exchangeImplementations(original, custom);
+    method_exchangeImplementations(original_open, custom_open);
 }
 
 - (BOOL)application:(UIApplication *)application insertDidFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
@@ -22,23 +25,12 @@
     return YES;
 }
 
-- (BOOL)application:(UIApplication *)app
-            openURL:(NSURL *)url
-            options:(NSDictionary<UIApplicationOpenURLOptionsKey,id> *)options;
-{
-    if ([[url scheme] containsString:@"insert"]) {
-        [[InsertManager sharedManager] initWithUrl:url];
-        return YES;
-    }
-
-    return YES;
-}
-
-- (BOOL)application:(UIApplication *)application openURL:(NSURL *)url sourceApplication:(NSString *)sourceApplication annotation:(id)annotation {
+- (BOOL)application:(UIApplication *)application insertOpenURL:(NSURL *)url sourceApplication:(NSString *)sourceApplication annotation:(id)annotation {
      if ([[url scheme] containsString:@"insert"]) {
         [[InsertManager sharedManager] initWithUrl:url];
-        return YES;
-    }
+         return YES;
+     }
+    [self application:application insertOpenURL:url sourceApplication:sourceApplication annotation:annotation];
     return YES;
 }
 @end
